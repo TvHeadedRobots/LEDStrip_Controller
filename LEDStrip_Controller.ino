4470bcd17ffea4047ed784d672e490861a6ea29b
Skip Tabor <pneusolematic@mac.com>
2012-06-24 22:31:37 -0400

    LEDStrip_Controller initial commit
    Software to help utilize an RGB LED Strips as lighting. It will flip
    through different lighting modes using an interrupt initiated by a
    button press. Still needs lots of work.
    
    To Do:
    Move modes into their own functions
    De-bounce the button

---
+++ LEDStrip_Controller.ino
@@ -0,0 +1,84 @@
+// color swirl! connect an RGB LED to the PWM pins as indicated
+// in the #defines
+// public domain, enjoy!
+ 
+#define REDPIN 6
+#define GREENPIN 5
+#define BLUEPIN 3
+//#define BUTTONPIN 2
+
+#define FADESPEED 5     // make this higher to slow down
+
+int mode = 0;
+ 
+void setup() {
+  Serial.begin(57600);
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+  //pinMode(BUTTONPIN, INPUT);
+  attachInterrupt(0, modeButton, RISING);
+
+}
+ 
+ 
+void loop() {
+  int r, g, b;
+  
+  if (mode == 0) {
+    Serial.print("mode: ");
+    Serial.println(mode);
+    analogWrite(REDPIN, 255);
+    analogWrite(GREENPIN, 255);
+    analogWrite(BLUEPIN, 255);
+  }
+  
+  else {
+    Serial.print("mode: ");
+    Serial.println(mode);
+    // fade from blue to violet
+    for (r = 0; r < 256; r++) { 
+      analogWrite(REDPIN, r);
+      delay(FADESPEED);
+    } 
+    // fade from violet to red
+    for (b = 255; b > 0; b--) { 
+      analogWrite(BLUEPIN, b);
+      delay(FADESPEED);
+    } 
+    // fade from red to yellow
+    for (g = 0; g < 256; g++) { 
+      analogWrite(GREENPIN, g);
+      delay(FADESPEED);
+    } 
+    // fade from yellow to green
+    for (r = 255; r > 0; r--) { 
+      analogWrite(REDPIN, r);
+      delay(FADESPEED);
+    } 
+    // fade from green to teal
+    for (b = 0; b < 256; b++) { 
+      analogWrite(BLUEPIN, b);
+      delay(FADESPEED);
+    } 
+    // fade from teal to blue
+    for (g = 255; g > 0; g--) { 
+      analogWrite(GREENPIN, g);
+      delay(FADESPEED);
+    }
+  } 
+}
+
+void modeButton() {
+  
+  // there are 2 modes
+  if (mode < 1) {
+    mode++;
+  }
+  else
+  {
+    mode = 0;
+  }
+  
+}
+
